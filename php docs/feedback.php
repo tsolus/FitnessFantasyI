4140e0d382ad24467ef4c373d8cfb10fae7a8cad
Ugo Demy <61797746+Nibleash@users.noreply.github.com>
2020-12-02 18:19:36 +0900

    Add files via upload

---
+++ php docs/feedback.php
@@ -0,0 +1,49 @@
+<?php 
+require './dbh.inc.php';
+
+$view = $_POST['view'];
+$username = $_POST['username'];
+$comments = $_POST['comments'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO Feedback (feedbackNumber, username, email, feedback, suggestion) VALUES ('', '$username', '$email', '$view', '$comments')";
+$query = mysqli_query($conn, $sql);
+
+echo '<script>alert("Thank You..! Your Feedback is Valuable to Us"); location.replace(document.referrer);</script>';
+
+
+
+/*$query1 = "SELECT * FROM `staff` WHERE email = '$email' AND password = '$password'";
+$result_query1 = mysqli_query($conn, $query1);
+$count_query1 = mysqli_num_rows($result_query1);
+/*if ($count_query1 != 0) 
+	{
+    header("Location: controller/staff.php");
+	exit();
+	} 
+else {
+		    $query2 = "SELECT * FROM `admin` WHERE email = '$email' AND password = '$password'";
+			$result_query2 = mysqli_query($conn, $query2);
+			$count_query2 = mysqli_num_rows($result_query2);
+			$count_query2 = mysqli_num_rows($result_query2);
+			if ($count_query2!=0) 
+			{
+		   		header("Location: controller/admin.php");
+				exit();
+		   	} 		
+		   	else
+		   	 {
+		   	 	echo '<script>alert("Incorrect Credentials Entered"); location.replace(document.referrer);</script>';
+
+			 }
+
+	}
+
+//echo $email;
+//echo '</br>';
+//echo $password;
+
+
+
+ob_end_flush();*/
+?>
